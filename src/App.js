1f815d74b7b7fb4326d27f62191ed6c275528db3
ANS <36783010+answebdev@users.noreply.github.com>
2023-10-13 17:29:13 -0700

    clean up code

--- src/App.js
+++ src/App.js
@@ -8,16 +8,11 @@ import Error from "./components/pages/Error";
 const App = () => {
   return (
     <Router>
-      {/* <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/about"> About </Link>
-      </nav> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Error />} />
       </Routes>
-      {/* <div> Foooter </div> */}
     </Router>
   );
 };
